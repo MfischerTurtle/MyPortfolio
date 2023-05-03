26a22b014194d3587a33b8c8e73af73ab0182a44
KungFuTurtle <m.rfischer5235@gmail.com>
2023-05-03 05:39:04 -0500

    fixing errors

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import '../src/style.css'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'tailwindcss/tailwind.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
