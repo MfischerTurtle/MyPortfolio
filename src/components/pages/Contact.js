788499088b02665e5c6031e78ddf9fd88b35bb61
KungFuTurtle <m.rfischer5235@gmail.com>
2023-05-25 02:40:00 -0500

    final changes

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,95 +1,20 @@
-import React, { useState } from 'react';
-//changes in the state varible changes in the dom
-import {validateEmail} from '../../utils/helpers'
 
+import React from 'react';
 
-function Contact() {
-  
-  // Here we set two state variables for firstName and lastName using `useState`
-  const [fullName, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [text, setText] = useState('');
-  const [message, setMessage] = useState('');
+const Contact = () => {
+    return (
+        <div>
+            <h1 id="about" className="section-title primary-border">Contact Information</h1>
+            <h2 className='hero-cta h2'>You can reach me via these channels:</h2>
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = e.target;
-
-    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
-    if(name === 'name'){
-      setName(value)
-    }
-    else if (name === 'email'){
-      setEmail(value)
-    }
-    else {
-      setText(value)
-    }
-  };
-
-  
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-  
-    if (fullName === '') {
-      setMessage('Name cannot be empty');
-      return;
-    }
-    if (email === '') {
-      setMessage('Email cannot be empty');
-      return;
-    }
-    if (text === '') {
-      setMessage('Text cannot be empty');
-      return;
-    }
-    if (!validateEmail(email)) {
-      setMessage('Invalid email, make sure to have @(domain name here).com');
-      return;
-    }
-  
-   
-  };
-  
-
-
-  return (
-    <><div>
-      <h1 id="about" className="section-title primary-border">
-        Contact Me
-      </h1>
-    </div>
-    <div className='contact'>
-    <form className="contact-form" onSubmit={handleFormSubmit}>
-  <input
-    value={fullName}
-    name="name"
-    onChange={handleInputChange}
-    type="text"
-    placeholder="Enter name here"
-  />
-  <input
-    value={email}
-    name="email"
-    onChange={handleInputChange}
-    type="text"
-    placeholder="Enter email here"
-  />
-  <textarea
-    value={text}
-    name='text'
-    onChange={handleInputChange}
-    type="text"
-    placeholder='Enter message here'>
-  </textarea>
-  <button type="submit">
-    Submit
-  </button>
-</form>
-        <p>{message}</p>
-      </div></>
-  );
+            <ul className='ulcontact'>
+                <li>Phone: <a href="tel:4805805351">4805805351</a></li>
+                <li>Email: <a href="mailto:mrfischer55@gmail.com">mrfischer55@gmail.com</a></li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/matthew-fischer-147740267/" target="_blank" rel="noopener noreferrer">Matthew Fischer's LinkedIn</a></li>
+            </ul>
+        </div>
+    )
 }
 
 export default Contact;
+
